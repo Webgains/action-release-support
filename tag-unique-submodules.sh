c119f4ae80b00191ba1404ba88c2bca366010731
Ethan Bray <ebray@webgains.com>
2022-01-18 15:38:23 +0000

    SRV-399 - Check that identical submodules point to the same hash. Only tag submodules once.

---
+++ tag-unique-submodules.sh
@@ -0,0 +1,71 @@
+##!/bin/bash
+
+# Describe the parameters required by this script
+while getopts p:v:t: flag
+do
+    case "${flag}" in
+        p) project_name=${OPTARG};;
+        v) version=${OPTARG};;
+        t) tag_name=${OPTARG};;
+        *) echo "Invalid flag supplied"; exit 1;
+    esac
+done
+
+export project_name
+export version
+export tag_name
+
+# Check whether the tag already exists in the submodule
+function tag_exists() {
+  TAG="${tag_name}_${version}"
+  if [ "$(git tag -l "$TAG")" ]
+  then
+    echo "Tag exists"
+    return 0
+  fi
+
+  echo "Tag does not exist"
+  return 1
+}
+
+# Tag the submodule and push those tags
+function tag_submodule_and_push_tags() {
+  echo "Tagging submodule"
+  git tag -m "${project_name} Release ${version}" "${tag_name}_${version}"
+  git push --tags
+}
+
+# Check whether any identical submodules point at different git hashes. If they do, we need to
+# stop the process early otherwise the git tags won't work correctly.
+function identical_submodules_point_at_same_hash() {
+  submodule_status=$(git submodule status)
+
+  # Count the unique submodules in use
+  unique_submodules=$(echo "$submodule_status" | awk '{ print $2 }' | awk -F/ '{ print $NF }' | uniq -c | wc -l | xargs)
+
+  # Count the unique submodule hashes
+  unique_hashes=$(echo "$submodule_status" | awk '{ print $1 }' | uniq -c | wc -l | xargs)
+
+  echo "There is ${unique_submodules} unique submodule(s) and ${unique_hashes} unique hash(es)"
+
+  # If the amount of unique hashes is different to the amount of unique submodules then it means at least one
+  # of the identical submodules has a different hash
+  if [[ $unique_submodules -eq $unique_hashes ]]
+  then
+    return 0
+  fi
+
+  return 1
+}
+
+export -f tag_exists
+export -f tag_submodule_and_push_tags
+export -f identical_submodules_point_at_same_hash
+
+if ! identical_submodules_point_at_same_hash
+then
+  echo "Identical submodules are not pointing at the same git hash"
+  exit 1
+fi
+
+git submodule foreach 'if ! tag_exists; then tag_submodule_and_push_tags; fi'
